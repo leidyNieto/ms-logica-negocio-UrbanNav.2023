0a72b1e35f3b060c573fa6bf9dd8581e7f5ee984
Leidy Johana <lj13.az50@gmail.com>
2023-11-22 18:29:05 -0500

    formas de pago

--- src/config/seguridad.config.ts
+++ src/config/seguridad.config.ts
@@ -10,6 +10,5 @@ export namespace configuracionSeguridad {
   export const mysqlConnectionString = process.env.MYSQL_CONNECTION_STRING;
   export const userMysql = process.env.USER_MYSQL;
   export const passwordMysql = process.env.PASSWORD_MYSQL;
-  export const  secret =process.env.STRIPE_SECRET;
 
 }
